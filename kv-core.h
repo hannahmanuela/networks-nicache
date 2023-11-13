e03dbbea24a32a4a54d13150ec87ae5bccb99c00
Hannah Gross <manuela@30-10-168.wireless.csail.mit.edu>
2023-11-13 15:33:49 -0500

    kiss

--- kv-core.h
+++ kv-core.h
@@ -1,15 +1,31 @@
 #ifndef KVCORE_H
 #define KVCORE_H
 
-Class KVCore {
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+class KVCore {
 
   public:
     KVCore() {
+      std::unordered_map<std::string, std::string>key_value_map_;
     }
+
+    std::string Get(std::string key) {
+      return key_value_map_[key];
+    };
+
+    void Put(std::string key, std::string value) {
+      key_value_map_.insert(key, value);
+    };
 
 
   private:
+    std::unordered_map<std::string, std::string> key_value_map_;
 
-}
 
+};
 
+#endif  // KVCORE_H
