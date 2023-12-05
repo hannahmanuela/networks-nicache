f0cfa8254fcc590a2e3d9f111c94d7501e4a575d
bencw12 <bencw12@node-0.hmng-179464.mit-aifm-pg0.clemson.cloudlab.us>
2023-12-05 06:56:35 +0000

    add timing back

--- src/client.rs
+++ src/client.rs
@@ -150,7 +150,7 @@ fn do_request(
         on_host = true;
         &host_conn
     };
-    // let time_after_get_addr_to_read = Instant::now();
+    let time_after_get_addr_to_read = Instant::now();
     // read value from appropriate source
     post_read_and_wait(
         conn_to_use.conn_id,
@@ -159,9 +159,9 @@ fn do_request(
         kv_addr.addr,
         conn_to_use.val_read_key,
     )?;
-    // let time_after_get_val = Instant::now();
-    // Ok((time_after_get_addr_to_read, time_after_get_val, on_host))
-    Ok((Instant::now(), Instant::now(), on_host))
+    let time_after_get_val = Instant::now();
+    Ok((time_after_get_addr_to_read, time_after_get_val, on_host))
+    //Ok((Instant::now(), Instant::now(), on_host))
 }
 
 fn run_latency(
@@ -177,33 +177,33 @@ fn run_latency(
     for _ in 0..num_iters {
         reqs.push(rng.gen_range(0..N_KEYS as u64));
     }
-    // let mut sum_get_addr_time: Duration = Duration::from_secs(0);
-    // let mut sum_get_val_time_soc: Duration = Duration::from_secs(0);
-    // let mut sum_get_val_time_host: Duration = Duration::from_secs(0);
-    // let mut num_host_iters = 0;
-    // let mut num_soc_iters = 0;
+    let mut sum_get_addr_time: Duration = Duration::from_secs(0);
+    let mut sum_get_val_time_soc: Duration = Duration::from_secs(0);
+    let mut sum_get_val_time_host: Duration = Duration::from_secs(0);
+    let mut num_host_iters = 0;
+    let mut num_soc_iters = 0;
     // do 10k requests and measure latency each time
     for offset in reqs {
-        // let now = Instant::now();
+        let now = Instant::now();
         // get address from index
         let (time_after_addr, time_after_val, on_host) = do_request(soc_conn, host_conn, addr_buf, val_buf, offset)?;
-        // let time_to_addr = time_after_addr - now;
-        // sum_get_addr_time = sum_get_addr_time + time_to_addr;
-        // let time_to_val = time_after_val - time_after_addr;
-        // if on_host {
-        //     sum_get_val_time_host = sum_get_val_time_host + time_to_val;
-        //     num_host_iters += 1;
-        // } else {
-        //     sum_get_val_time_soc = sum_get_val_time_soc + time_to_val;
-        //     num_soc_iters += 1;
-        // }
+        let time_to_addr = time_after_addr - now;
+        sum_get_addr_time = sum_get_addr_time + time_to_addr;
+        let time_to_val = time_after_val - time_after_addr;
+        if on_host {
+            sum_get_val_time_host = sum_get_val_time_host + time_to_val;
+            num_host_iters += 1;
+        } else {
+            sum_get_val_time_soc = sum_get_val_time_soc + time_to_val;
+            num_soc_iters += 1;
+        }
     }
-    // let avg_get_addr_time = sum_get_addr_time /  num_iters;
-    // let avg_get_val_time_soc = sum_get_val_time_soc / num_soc_iters;
-    // let avg_get_val_time_host = sum_get_val_time_host / num_host_iters;
-    // println!("avg_get_addr_time in nanos: {}", avg_get_addr_time.as_nanos());
-    // println!("avg_get_val_time_soc in nanos: {}", avg_get_val_time_soc.as_nanos());
-    // println!("avg_get_val_time_host in nanos: {}", avg_get_val_time_host.as_nanos());
+    let avg_get_addr_time = sum_get_addr_time /  num_iters;
+    let avg_get_val_time_soc = sum_get_val_time_soc / num_soc_iters;
+    let avg_get_val_time_host = sum_get_val_time_host / num_host_iters;
+    println!("avg_get_addr_time in nanos: {}", avg_get_addr_time.as_nanos());
+    println!("avg_get_val_time_soc in nanos: {}", avg_get_val_time_soc.as_nanos());
+    println!("avg_get_val_time_host in nanos: {}", avg_get_val_time_host.as_nanos());
     Ok(())
 }
 
