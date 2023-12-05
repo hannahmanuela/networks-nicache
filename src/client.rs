3ec69062eccbcb03d947c9a5f25e44458473f2ed
bencw12 <bencw12@node-0.hmng-179464.mit-aifm-pg0.clemson.cloudlab.us>
2023-12-05 18:51:38 +0000

    this makes more sense

--- src/client.rs
+++ src/client.rs
@@ -188,8 +188,7 @@ fn run_latency(
     let mut avg_get_val_time_soc: Duration = Duration::from_secs(0);
     let mut avg_get_val_time_host: Duration = Duration::from_secs(0);
     // // do 10k requests and measure latency each time
-    for mut offset in 0..MEM_SIZE  as u64{
-	offset = offset * (4096/8);
+    for mut offset in 0..N_KEYS as u64/64 {
         let now = Instant::now();
         // get address from index
         let (time_after_addr, time_after_val, on_host) = do_request(soc_conn, host_conn, addr_buf, val_buf, offset)?;
